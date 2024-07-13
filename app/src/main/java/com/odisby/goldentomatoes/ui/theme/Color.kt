4f5849ac045351e202ad5703fbe88dbade65fcc9
Ruliam dos Santos de Oliveira <ruliam.oliveira@gmail.com>
2024-07-13 14:39:51 -0300

    feat(ui): Creating my theme

--- app/src/main/java/com/odisby/goldentomatoes/ui/theme/Color.kt
+++ app/src/main/java/com/odisby/goldentomatoes/ui/theme/Color.kt
@@ -2,10 +2,30 @@ package com.odisby.goldentomatoes.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val Primary900 = Color(0xFFd85200)
+val Primary800 = Color(0xFFe26b00)
+val Primary700 = Color(0xFFe87a00)
+val Primary600 = Color(0xFFee8a02)
+val Primary500 = Color(0xFFf19504)
+val Primary400 = Color(0xFFf3a427)
+val Primary300 = Color(0xFFf5b44d)
+val Primary200 = Color(0xFFf8ca80)
+val Primary100 = Color(0xFFfadfb2)
+val Primary50 = Color(0xFFfdf2e0)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val Secondary900 = Color(0xFF042663)
+val Secondary800 = Color(0xFF113779)
+val Secondary700 = Color(0xFF1a4085)
+val Secondary600 = Color(0xFF244a91)
+val Secondary500 = Color(0xFF2b519a)
+val Secondary400 = Color(0xFF4e6aa7)
+val Secondary300 = Color(0xFF6f84b6)
+val Secondary200 = Color(0xFF97a7cb)
+val Secondary100 = Color(0xFFc0c9e0)
+val Secondary50 = Color(0xFFe6eaf2)
+
+val TertiaryMain = Color(0xFFFFFFFF)
+val White_80 = Color(0xCCFFFFFF)
+val White_50 = Color(0x80FFFFFF)
+
+val BackgroundColor = Color(0xFF181818)
