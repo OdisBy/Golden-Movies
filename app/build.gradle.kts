261b66c95d660eb81bd9dd186f0bf382eef8bcc5
Ruliam dos Santos de Oliveira <ruliam.oliveira@gmail.com>
2024-07-13 14:53:31 -0300

    chore(deps): Adding ui and home modules to app

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,6 +50,8 @@ android {
 }
 
 dependencies {
+    implementation(project(":core:ui"))
+    implementation(project(":feature:home"))
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
