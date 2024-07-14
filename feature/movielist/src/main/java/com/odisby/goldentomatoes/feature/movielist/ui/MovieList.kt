4c1e6b5abade7f5d386386a8d26aa1e88c3e1c1f
Ruliam dos Santos de Oliveira <ruliam.oliveira@gmail.com>
2024-07-14 14:20:37 -0300

    feat(movie list): Adding top app bar

--- feature/movielist/src/main/java/com/odisby/goldentomatoes/feature/movielist/ui/MovieList.kt
+++ feature/movielist/src/main/java/com/odisby/goldentomatoes/feature/movielist/ui/MovieList.kt
@@ -16,52 +16,74 @@ import androidx.compose.foundation.lazy.staggeredgrid.LazyVerticalStaggeredGrid
 import androidx.compose.foundation.lazy.staggeredgrid.StaggeredGridCells
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.KeyboardArrowLeft
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.IconButtonDefaults
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.rememberTopAppBarState
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.rememberVectorPainter
+import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.odisby.goldentomatoes.core.ui.theme.BackgroundColor
-import com.odisby.goldentomatoes.core.ui.theme.Black_50
 import com.odisby.goldentomatoes.core.ui.theme.GoldenTomatoesTheme
 import com.odisby.goldentomatoes.core.ui.theme.TextColor
 import com.odisby.goldentomatoes.feature.movielist.R
 
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MovieListRoot(
     navigateUp: () -> Unit,
     navigateToDetailsScreen: (Long) -> Unit,
+    listType: String = "discover",
     modifier: Modifier = Modifier,
     viewModel: MovieListViewModel = MovieListViewModel()
 ) {
+    val scrollBehavior = TopAppBarDefaults.pinnedScrollBehavior(rememberTopAppBarState())
+
+
     Scaffold(
         modifier = Modifier
             .background(BackgroundColor)
             .statusBarsPadding()
-            .navigationBarsPadding(),
+            .navigationBarsPadding()
+            .nestedScroll(scrollBehavior.nestedScrollConnection),
         topBar = {
-            IconButton(
-                onClick = navigateUp,
-                modifier = Modifier.statusBarsPadding(),
-                colors = IconButtonDefaults.iconButtonColors(
-                    contentColor = TextColor,
-                    containerColor = Black_50,
-                )
-            ) {
-                Icon(
-                    painter = rememberVectorPainter(Icons.AutoMirrored.Filled.KeyboardArrowLeft),
-                    contentDescription = "Voltar"
-                )
-            }
+            CenterAlignedTopAppBar(
+                title = {
+                    Text(
+                        "Descobrir Filmes",
+                        color = TextColor,
+                        maxLines = 1,
+                        overflow = TextOverflow.Ellipsis
+                    )
+                },
+                navigationIcon = {
+                    IconButton(
+                        onClick = navigateUp,
+                        modifier = Modifier.statusBarsPadding(),
+                        colors = IconButtonDefaults.iconButtonColors(
+                            contentColor = TextColor,
+                        )
+                    ) {
+                        Icon(
+                            painter = rememberVectorPainter(Icons.AutoMirrored.Filled.KeyboardArrowLeft),
+                            contentDescription = "Voltar"
+                        )
+                    }
+                },
+                scrollBehavior = scrollBehavior,
+            )
         }
     ) { contentPadding ->
         MovieListScreen(
@@ -208,6 +230,7 @@ fun MovieListScreen(
 }
 
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Preview
 @Composable
 private fun MovieListScreenPreview() {
@@ -218,19 +241,30 @@ private fun MovieListScreenPreview() {
                 .statusBarsPadding()
                 .navigationBarsPadding(),
             topBar = {
-                IconButton(
-                    onClick = { },
-                    modifier = Modifier.statusBarsPadding(),
-                    colors = IconButtonDefaults.iconButtonColors(
-                        contentColor = TextColor,
-                        containerColor = Black_50,
-                    )
-                ) {
-                    Icon(
-                        painter = rememberVectorPainter(Icons.AutoMirrored.Filled.KeyboardArrowLeft),
-                        contentDescription = "Voltar"
-                    )
-                }
+                CenterAlignedTopAppBar(
+                    title = {
+                        Text(
+                            "Descobrir Filmes",
+                            color = TextColor,
+                            maxLines = 1,
+                            overflow = TextOverflow.Ellipsis
+                        )
+                    },
+                    navigationIcon = {
+                        IconButton(
+                            onClick = { },
+                            modifier = Modifier.statusBarsPadding(),
+                            colors = IconButtonDefaults.iconButtonColors(
+                                contentColor = TextColor,
+                            )
+                        ) {
+                            Icon(
+                                painter = rememberVectorPainter(Icons.AutoMirrored.Filled.KeyboardArrowLeft),
+                                contentDescription = "Voltar"
+                            )
+                        }
+                    }
+                )
             }
         ) { contentPadding ->
             MovieListScreen(
