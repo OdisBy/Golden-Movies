20c98d7187a692f0c65854fa6c0418ea82c73461
Ruliam dos Santos de Oliveira <ruliam.oliveira@gmail.com>
2024-08-31 09:26:03 -0300

    feat: Testing pipeline

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -96,7 +96,6 @@ dependencies {
     implementation(project(":data:remote"))
     implementation(project(":data:data"))
 
-
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
